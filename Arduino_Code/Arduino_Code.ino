f6d393024c9ab2366b56d2288388f9ad23ab12f9
Paul Z <paul@zechs.de>
2023-05-22 11:50:13 +0200

    Sending, Receiving Commands working

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -6,28 +6,133 @@
 #define Relais_DC_to_WR     10
 #define Relais_WR_to_AC     11
 
-// ADS1115 Pins
-#define ADS1115_SDA 18
-#define ADS1115_SCL 19
+// ADS1115
+#include<ADS1115_WE.h> 
+#include<Wire.h>
+#define I2C_ADDRESS 0x48
+ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 
 // PWM Output
 #define PWM_NT 3
 #define PWM_DC 5
 
-// Funktionsdefinitionen
-void off(void); 
+// Incoming Communication
+boolean newData = false;
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];
+char commandFromESP[numChars] = {0};
+float powerFromESP = 0.0;
+
+// Outgoing Communication
+String Status = "Off";
+float uNT = 0.0;
+float uBatt = 0.0;
+float uWR = 0.0;
+float iBatt = 0.0;
 
 
 void setup(){
     Serial.begin(115200);
+    Wire.begin();
+    adc.init();
+    adc.setVoltageRange_mV(ADS1115_RANGE_6144);
 }
 
 
 void loop(){
+    getCommand();
+    measurement();
+    returnData();
     delay(1000);
 }
 
 
-void off(){
-    delay(1000);
+void getCommand() {
+    recvWithStartEndMarkers();
+    if (newData == true) {
+        strcpy(tempChars, receivedChars);
+        parseData();
+        showParsedData();
+        newData = false;
+    }
 }
+
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0';
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+
+void parseData() {
+
+    char * strtokIndx;
+
+    strtokIndx = strtok(tempChars,",");
+    strcpy(commandFromESP, strtokIndx);     
+
+    strtokIndx = strtok(NULL, ",");
+    powerFromESP = atof(strtokIndx);
+
+}
+
+
+void showParsedData() {
+    Serial.print("Message: ");
+    Serial.println(commandFromESP);
+    Serial.print("Power: ");
+    Serial.println(powerFromESP);
+}
+
+
+void measurement() {
+    uNT = readChannel(ADS1115_COMP_0_GND);
+    uBatt = readChannel(ADS1115_COMP_1_GND);
+    uWR = readChannel(ADS1115_COMP_2_GND);
+    iBatt = readChannel(ADS1115_COMP_3_GND);
+}
+
+
+float readChannel(ADS1115_MUX channel){
+  float voltage = 0.0;
+  adc.setCompareChannels(channel);
+  adc.startSingleMeasurement();
+  while(adc.isBusy()){}
+  voltage = adc.getResult_mV();
+  return voltage;
+}
+
+
+void returnData() {
+    String outgoingData = "<" + Status + "," + String(uNT) + "," + String(uBatt) + "," + String(uWR) + "," + String(iBatt) + ">";
+    Serial.print(outgoingData);
+}
+
--- ESP_Code/ESP_Code.ino
+++ ESP_Code/ESP_Code.ino
@@ -1,8 +1,124 @@
-void setup(){
+// Incoming Communication
+boolean newData = false;
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];
+char statusFromArduino[numChars] = {0};
+float uNT = 0;
+float uBatt = 0.0;
+float uWR = 0.0;
+float iBatt = 0.0;
+
+// Outgoing Communication
+String command = "Off";
+float power = 0.0;
+
+
+void setup() {
     Serial.begin(115200);
 }
 
 
-void loop(){
+void loop() {
+    getPower();
+    sendCommand();
+    getMeasurements();
+    sendToServer();
     delay(1000);
+}
+
+
+void getPower() {
+
+}
+
+
+void sendCommand() {
+    String outgoingData = "<" + command + "," + String(power) + ">";
+    Serial.print(outgoingData);
+}
+
+
+void getMeasurements() {
+    recvWithStartEndMarkers();
+    if (newData == true) {
+        strcpy(tempChars, receivedChars);
+        parseData();
+        showParsedData();
+        newData = false;
+    }
+}
+
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0';
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+
+void parseData() {
+
+    char * strtokIndx;
+
+    strtokIndx = strtok(tempChars,",");
+    strcpy(statusFromArduino, strtokIndx);
+ 
+    strtokIndx = strtok(NULL, ",");
+    uNT = atof(strtokIndx);
+
+    strtokIndx = strtok(NULL, ",");
+    uBatt = atof(strtokIndx); 
+
+    strtokIndx = strtok(NULL, ",");
+    uWR = atof(strtokIndx);
+
+    strtokIndx = strtok(NULL, ",");
+    iBatt = atof(strtokIndx);
+
+}
+
+
+void showParsedData() {
+    Serial.print("Status: ");
+    Serial.println(statusFromArduino);
+    Serial.print("uNT: ");
+    Serial.println(uNT);
+    Serial.print("uBatt: ");
+    Serial.println(uBatt);
+    Serial.print("uWR: ");
+    Serial.println(uWR);
+    Serial.print("iBatt: ");
+    Serial.println(iBatt);
+}
+
+
+void sendToServer() {
+
 }
